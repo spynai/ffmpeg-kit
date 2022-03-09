8c50a47eb340348fb88859410dbacd6223c26d61
Sabarinathan <nathans441@gmail.com>
2022-03-10 01:02:13 +0530

    testing require js file

--- flutter/ffmpeg_kit_flutter_web/web/image_2_video.js
+++ flutter/ffmpeg_kit_flutter_web/web/image_2_video.js
@@ -1,16 +1,12 @@
-require(['require', 'build/flutter_assets/packages/ffmpeg_kit_flutter_web/web/@ffmpeg/core/dist/ffmpeg-core.js'], function (require) {
-    var namedModule = require('build/flutter_assets/packages/ffmpeg_kit_flutter_web/web/@ffmpeg/core/dist/ffmpeg-core.js');
-    const { createFFmpeg, fetchFile } = namedModule;
+// const { myCreateFFmpeg, fetchFile } = require('./@ffmpeg/core/dist/ffmpeg-core.js');;
+// const ffmpeg = myCreateFFmpeg({ log: true });
+
+requirejs(['require', './@ffmpeg/core/dist/ffmpeg-core.js'], function (require) {
+    var namedModule = requirejs('./@ffmpeg/core/dist/ffmpeg-core.js');
+    const { createFFmpeg } = namedModule;
     const ffmpeg = createFFmpeg({ log: true });    
     let a =5;
 });
-
-// require(['require', 'build/flutter_assets/packages/ffmpeg_kit_flutter_web/web/@ffmpeg/ffmpeg/dist/ffmpeg.min.js'], function (require) {
-//     var namedModule = require('build/flutter_assets/packages/ffmpeg_kit_flutter_web/web/@ffmpeg/ffmpeg/dist/ffmpeg.min.js');
-//     const { createFFmpeg, fetchFile } = namedModule;
-//     const ffmpeg = createFFmpeg({ log: true });    
-//     let a =5;
-// });
 
 // require(['require', '/Users/sabarinathanbalakumar/vscodeprojects/spyn/ffmpeg.wasm/dist/ffmpeg.dev.js'], function (require) {
 //     var namedModule = require('/Users/sabarinathanbalakumar/vscodeprojects/spyn/ffmpeg.wasm/dist/ffmpeg.dev.js');
